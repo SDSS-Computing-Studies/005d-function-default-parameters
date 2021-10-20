b95dbaf0b2f76dc92cb552f20422026c18131f18
hungrybeagle <cyang@deltalearns.ca>
2021-10-20 13:38:49 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,8 +6,11 @@ greeting: string that will contain a salutation, examples: Hello, Hiya, Ola
 name: a string that will contain a person's name.  This should have a default value of "Benjamin"
 question: a string that will contain a full question to ask the person.  It should have a default value of "How are you"
 
-Have the greeting, name and question combine together into a string. Note: You are not using a print statement, so you must
+Have the greeting, name and question combine together into a string. 
+Note: You are not using a print statement, so you may
 use the concatenate (+) operator. Make sure your spacing is appropriate.
+However, you can still use formatted strings using:
+f"{variable} contents"
 
 return value:
 string. The output sentence
